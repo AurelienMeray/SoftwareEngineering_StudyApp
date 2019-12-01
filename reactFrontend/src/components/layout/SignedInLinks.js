0b834f6527a9bd5ae6abbb7e779685ce7acba66d
lcani006 <lcani006@fiu.edu>
2019-11-30 22:34:27 -0500

    updated ignore

---
+++ reactFrontend/src/components/layout/SignedInLinks.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const SignedInLinks = () => {
+    return(  
+        <ul className="right">
+            <li><NavLink to='/createroom'>Create Room</NavLink></li>
+            <li><NavLink to='/searchrooms'>Search Room</NavLink></li>
+            <li><NavLink to='/'>Logout</NavLink></li>
+            <li><NavLink to='/' className='btn btn-floating blue lighten-2>'>Prof</NavLink></li>
+        </ul>
+    )
+}
+
+export default SignedInLinks
